90eccc89e6200717fe84b671973b0b9fd8f6566f
Rohan Saxena <saxenarohan97@gmail.com>
2017-11-09 11:46:06 +0530

    Add files via upload

--- pre.py
+++ pre.py
@@ -1,14 +1,17 @@
 from problem1 import *
 import config
-import csv
 
-# ----------
 
 '''
-# Preprocessing the data for ARM:
 
-# 1. Finding the hour number (As an hour number where 1 is 4 pm)
-# 2. Finding the student's group (First year, second year, etc)
+Preprocessing the data for ARM:
+
+1. Finding the hour number (As an hour number where 1 is 4 pm)
+2. Finding the student's group (First year, second year, etc)
+
+'''
+
+import csv
 
 monthFileList = [open('Dataset/augSales.csv'), open('Dataset/sepSales.csv'), open('Dataset/octSales.csv'), open('Dataset/novSales.csv')]
 monthFileNameList = ['Dataset/augSales.csv', 'Dataset/sepSales.csv', 'Dataset/octSales.csv', 'Dataset/novSales.csv']
@@ -19,11 +22,20 @@ config.minconf = float(raw_input('Enter MinConf:'))
 
 P1.runFunc1OnFiles(monthFileList, monthWisePricelistFile, monthFileNameList)
 
-'''
+# -----------
 
-# ----------
+with open("Dataset/newPrices.csv") as newPriceFile:
 
-# P1.readNewPrices()
-P1.calculateProfit()
+    newPriceReader = csv.reader(newPriceFile, delimiter=',')
+    headRead = True
 
-# ----------
+    for line in newPriceReader:
+        
+        if headRead:
+            header = line
+            print header
+            headRead = False
+        else:
+            
+
+# -----------
