dde8a6e25abe9ce53e89d995daecb749d3f0142f
nmakes <nav.naveenvenkat@gmail.com>
2017-11-12 19:33:56 +0530

    making p2

---
+++ problem2.py
@@ -0,0 +1,126 @@
+import csv
+import config
+
+monthFile = open("p2/novFinal.csv")
+
+reader = csv.reader(monthFile, delimiter=',')
+
+def extractData(reader):
+	retList = []
+
+	for line in reader:
+		
+		l = eval(line[1])
+		intL = []
+
+		for itemID in l:
+			intL.append(int(itemID))
+
+		retList.append(intL)
+
+	retList.sort()
+	return retList
+
+support_count = {}
+
+for itemset in extractData(reader):
+	
+	if association in support_count.keys():
+		support_count[itemset] = 
+
+	print associations
+
+
+
+
+
+class P2:
+
+	@staticmethod
+	def runARMOnFiles(monthFileList, monthWisePricelistFile, monthFileNameList = None):
+
+		print "ASSOCIATION RULES"
+		print "[item1, item2 .. item n] -> item"
+
+		print
+		print "minsup: ", config.minsup, "%"
+		print "minconf: ", config.minconf, "%"
+		print "maxsup: ", config.maxsup, "%"
+		print "maxconf: ", config.maxconf, "%"
+		print
+
+		for i in range(len(monthFileList)):
+			print "Running on file: ", monthFileNameList[i]
+			print
+			monthWisePricelistFile.seek(0,0)
+			P2.Func1(monthFileList[i], monthWisePricelistFile)
+			print "--x--x--"
+			print
+
+
+	@staticmethod
+	def ARM(monthFile, monthWisePricelistFile):	
+		'''
+			Obtain associations between items:
+
+				[item1 , item2 , ...] -> itemX
+
+			where,
+			the LHS contains 1 or more items, and RHS contains 1 item
+			This association will find items bought together. This means, items that have been bought by a person, in a single bill.
+		'''
+
+		file_output = []
+
+		reader = csv.reader(monthFile, delimiter=',')
+
+		next(reader)
+
+		support_count = {}
+		support_count_ant = {}
+		price_dict = {}
+
+		total_entries = 0
+
+		for i in reader:
+			itemID = i[1]
+			date = "CONTINUE CODE FROM HERE"
+
+			total_entries += 1
+
+			price_dict[itemID] = price
+
+			if((group, absHour, itemID) in support_count.keys()):
+				support_count[(group, absHour, itemID)] += 1
+				if int(itemID) > 1400:
+					print i
+			else:
+				support_count[(group, absHour, itemID)] = 1
+
+			if (group, absHour) in support_count_ant.keys():
+				support_count_ant[(group, absHour)] += 1
+			else:
+				support_count_ant[(group, absHour)] = 1
+
+		priceListReader = csv.reader(monthWisePricelistFile, delimiter=',')
+		next(priceListReader)
+
+		items = {}
+
+		for i in priceListReader:
+			items[i[0]] = i[1]
+
+		outputCount = 0
+
+		for keys in support_count.keys():
+			
+			(group, absHour, itemID) = keys
+			sup = float(support_count[keys]) * 100 / float(total_entries)
+			conf = float( float(support_count[keys]) * 100 / float(support_count_ant[(group, absHour)]))
+
+			if ( sup > config.minsup ) and ( sup < config.maxsup ) and ( conf > config.minconf ) and ( conf < config.maxconf ):
+				outputCount += 1
+				print group, ",", P1.getTimeSlot(absHour), " -> ", items[itemID],
+				print " : s=", str(sup), "% | c=", conf, "%"
+
+		print "\n Total outputCount: ", outputCount
